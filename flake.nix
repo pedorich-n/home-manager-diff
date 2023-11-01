885205d9ff1a94ae19679ee49e6d6737b99c221a
Nikita Pedorich <pedorich.n@gmail.com>
2023-11-01 23:34:14 +0900

    [flake] Update flake.lock and fix poetry2nix overlay

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
     perSystem = { pkgs, system, ... }: {
       _module.args.pkgs = import inputs.nixpkgs {
         inherit system;
-        overlays = [ inputs.poetry2nix.overlay ];
+        overlays = [ inputs.poetry2nix.overlays.default ];
       };
 
       packages = {
